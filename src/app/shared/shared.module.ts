9f061825dceea0efb48ff7b87dcc33e542d1f70f
Nayanne Batista <nayannelopes.bd@gmail.com>
2023-09-27 10:12:22 -0300

    Aula 1 após gravação

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
 import { BannerComponent } from "./banner/banner.component";
 import { BotaoControleComponent } from "./botao-controle/botao-controle.component";
 import { CardBuscaComponent } from "./card-busca/card-busca.component";
@@ -10,41 +8,43 @@ import { ContainerComponent } from "./container/container.component";
 import { DropdownUfComponent } from "./dropdown-uf/dropdown-uf.component";
 import { FooterComponent } from "./footer/footer.component";
 import { FormBaseComponent } from "./form-base/form-base.component";
-import { FormBuscaComponent } from "./form-busca/form-busca.component";
-import { HeaderComponent } from "./header/header.component";
-import { ModalComponent } from "./modal/modal.component";
-import { SeletorPassageiroComponent } from "./seletor-passageiro/seletor-passageiro.component";
-import { PassagemComponent } from "./passagem/passagem.component";
-import { PassagemDestaqueComponent } from "./passagem-destaque/passagem-destaque.component";
-import { MaterialModule } from "../core/material/material.module";
 import { CompanhiasComponent } from "./form-busca/filtros-complementares/companhias/companhias.component";
 import { FiltrosComplementaresComponent } from "./form-busca/filtros-complementares/filtros-complementares.component";
 import { LabelComponent } from "./form-busca/filtros-complementares/label/label.component";
 import { ParadasComponent } from "./form-busca/filtros-complementares/paradas/paradas.component";
 import { PrecosComponent } from "./form-busca/filtros-complementares/precos/precos.component";
+import { FormBuscaComponent } from "./form-busca/form-busca.component";
+import { HeaderComponent } from "./header/header.component";
+import { ModalComponent } from "./modal/modal.component";
+import { PassagemDestaqueComponent } from "./passagem-destaque/passagem-destaque.component";
+import { PassagemComponent } from "./passagem/passagem.component";
+import { SeletorPassageiroComponent } from "./seletor-passageiro/seletor-passageiro.component";
+import { CommonModule } from "@angular/common";
+import { MaterialModule } from "../core/material/material.module";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
+    HeaderComponent,
     BannerComponent,
     CardComponent,
+    ContainerComponent,
+    FooterComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    ContainerComponent,
-    DropdownUfComponent,
-    FooterComponent,
-    FormBaseComponent,
     FormBuscaComponent,
-    HeaderComponent,
-    SeletorPassageiroComponent,
     ModalComponent,
     BotaoControleComponent,
+    DropdownUfComponent,
+    SeletorPassageiroComponent,
+    FormBaseComponent,
     PassagemComponent,
-    PassagemDestaqueComponent,
     ParadasComponent,
     CompanhiasComponent,
     PrecosComponent,
     LabelComponent,
-    FiltrosComplementaresComponent
+    FiltrosComplementaresComponent,
+    PassagemDestaqueComponent
   ],
   imports: [
     CommonModule,
@@ -52,26 +52,26 @@ import { PrecosComponent } from "./form-busca/filtros-complementares/precos/prec
     ReactiveFormsModule
   ],
   exports: [
+    HeaderComponent,
     BannerComponent,
     CardComponent,
+    ContainerComponent,
+    FooterComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    ContainerComponent,
-    DropdownUfComponent,
-    FooterComponent,
-    FormBaseComponent,
     FormBuscaComponent,
-    HeaderComponent,
-    SeletorPassageiroComponent,
     ModalComponent,
     BotaoControleComponent,
+    DropdownUfComponent,
+    SeletorPassageiroComponent,
+    FormBaseComponent,
     PassagemComponent,
-    PassagemDestaqueComponent,
     ParadasComponent,
     CompanhiasComponent,
     PrecosComponent,
     LabelComponent,
     FiltrosComplementaresComponent,
+    PassagemDestaqueComponent
   ]
 })
 export class SharedModule { }
